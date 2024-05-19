106535aca5bb80c6282bba1514ae60e7ce64e126
NathanMarts <natanlop93@gmail.com>
2024-05-19 18:17:43 -0300

    alpine data

---
+++ app/static/app.js
@@ -0,0 +1,49 @@
+function data() {
+  return {
+    modal: {
+      show: false,
+      open() { this.show = true },
+      close() { this.show = false },
+      isOpen() { return this.show },
+    },
+    toastService: {
+      toasts: [],
+      error(message) {
+        const toast = {
+          type: 'error',
+          message: message,
+          visible: false,
+        }
+        this.toasts.push(toast);
+        setTimeout(() => {
+          this.toasts[this.toasts.length - 1].visible = true;
+        }, 10);
+        setTimeout(() => {
+          this.removeToast(0)
+        }, 3000);
+      },
+      success(message) {
+        const toast = {
+          type: 'success',
+          message: message,
+          visible: false,
+        }
+        this.toasts.push(toast);
+        setTimeout(() => {
+          this.toasts[this.toasts.length - 1].visible = true;
+        }, 10);
+        setTimeout(() => {
+          this.removeToast()
+        }, 3010);
+      },
+      removeToast(index) {
+        if (this.toasts.length > 0) {
+          this.toasts[index].visible = false;
+          setTimeout(() => {
+            this.toasts.shift()
+          }, 300);
+        }
+      }
+    }
+  }
+}
